99bdf451b964e77c8361e6f1d3c64b693d192fdf
PedrinhowVieira <pedro.dmvieira@gmail.com>
2021-01-26 16:32:52 -0300

    last class

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UserRepository } from './user.repository';
+import { JwtStrategy } from './jwt.strategy';
 
 
 @Module({
@@ -19,6 +20,13 @@ import { UserRepository } from './user.repository';
     TypeOrmModule.forFeature([UserRepository]),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    JwtStrategy,
+  ],
+  exports: [
+    JwtStrategy,
+    PassportModule,
+  ],
 })
 export class AuthModule {}
